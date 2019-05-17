3c1ddf8eabf87fb1712f598247f79c729ff4e1c3
agrabulosa <50594214+agrabulosa@users.noreply.github.com>
2019-05-17 15:28:37 +0200

    Create Main-parells.java

---
+++ Main-parells.java
@@ -0,0 +1,19 @@
+package parells;
+
+public class Main {
+	
+	int n = 0;
+	
+	public void main() {
+		
+		for(int i = 0; i < 200; i++)
+		{
+			if ( n % 2 == 0)
+				System.Out.println(n);
+			
+			n++;
+		}
+			
+	}
+}
+	
