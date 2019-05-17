abb3fd4d6b0e9990751006d5342e68a6128238e0
agrabulosa <50594214+agrabulosa@users.noreply.github.com>
2019-05-17 15:35:53 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,40 +1,40 @@
 package omplirnumeros;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		int n=5;
 		int[] taula = new int[n];
-		
+
 		taula = omplir(n);
 		int suma = sumar(taula);
-		
-		System.out.println("La suma és: " + suma);
+
+		System.out.println("La suma es: " + suma);
 
 	}
-	
-	
+
+
 	private static int[] omplir(int n) {
-		
+
 		int[] taula;
-		
+
 		taula = new int[n];
-		
+
 		for (int i = 0; i < n; i++)
-			taula[i] = i+i+1;
-		
-		return taula; 
+			taula[i] = i+i+2;
+
+		return taula;
 	}
-	
-	
+
+
 	private static int sumar(int[] taula) {
-	
+
 		int suma = 0;
-		
+
 		for (int i = 0; i < taula.length; i++)
 			suma = suma + taula[i];
-		
+
 		return suma;
 	}
-	
+
 }
